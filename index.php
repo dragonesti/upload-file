4ae43c9230862b7ce4fa67e6b183ad1ca3f8153f
Marcin Jóźwiak <dragonesti@poczta.onet.pl>
2022-02-15 11:12:39 +0100

    change file structure and names

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('inc/send_form.php');
+require_once('inc/upload_file.php');
 ?>
 
 
@@ -26,39 +26,6 @@ require_once ('inc/send_form.php');
 		<div class="contact__form">
 			<h3 class="contact__form--header">Formularz kontaktowy</h3>
 			<hr class="dec">
-<!--			<form action="" method="post" id="my_form_id" enctype="multipart/form-data" >-->
-<!--				<div class="form__group field">-->
-<!--					<input type="text" class="form__field" placeholder="Imię i nazwisko:" name="person" id="person"/>-->
-<!--					<label for="person" class="form__label">* Imię i nazwisko:</label>-->
-<!--				</div>-->
-<!--				<div class="form__group field">-->
-<!--					<input type="email" class="form__field" placeholder="* Email:" name="email" id="email"/>-->
-<!--					<label for="email" class="form__label">* Email:</label>-->
-<!--				</div>-->
-<!--				<div class="form__group field">-->
-<!--					<input type="tel" class="form__field" placeholder="Telefon:" name="tel" id="tel"/>-->
-<!--					<label for="tel" class="form__label">Telefon:</label>-->
-<!--				</div>-->
-<!--				<div class="form__group field">-->
-<!--					<textarea class="form__field" placeholder="* Wiadomość:" name="message" id="message"/> </textarea>-->
-<!--					<label for="message" class="form__label">* Wiadomość:</label>-->
-<!--				</div>-->
-<!--				<div class="form__group_file field">-->
-<!--					<label for="tel" class="form__label">Dodaj plik:</label>-->
-<!--					<input type="file" class="custom-file-input"  name="file" id="file"/>-->
-<!--				</div>-->
-
-
-<!--				<input type="checkbox" id="reg">-->
-<!--				<br><input type="checkbox" id="agree" /><label class="for_agree" for="agree" id="agree_cb"> * Wyrażam zgodę na przetwarzanie powyższych danych w celu otrzymania odpowiedzi na przesłany formularz. Podanie danych jest dobrowolne, ale niezbędne do otrzymania odpowiedzi.</label>-->
-<!--				<br>-->
-<!--				<div id="formanswer"></div>-->
-<!--				<div class="form_buttons">-->
-<!--					<input type="submit" class="send" value="Wyślij">-->
-<!--					<input type="reset" class="reset" id="reset_btn" value="Kasuj">-->
-<!--				</div>-->
-<!--			</form>-->
-
 
 			<form id="data" method="post" enctype="multipart/form-data" action="">
 				<input name="file" type="file" />
@@ -67,12 +34,6 @@ require_once ('inc/send_form.php');
 		</div>
 	</div>
 </section>
-
-<!--<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>-->
-
-<!--<script src="js/jquery-3.4.1.min.js"></script>-->
-<!--<script src="js/mail.js"></script>-->
-
 
 </body>
 </html>
